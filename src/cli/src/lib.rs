69e4b22b6a14f9a1675150d96b82526e2d309ab8
CBColleg3 <cbcolleg@udel.edu>
2024-05-09 15:15:53 -0400

    Working on parse_cli to extract image name

--- src/cli/src/lib.rs
+++ src/cli/src/lib.rs
@@ -1,5 +1,5 @@
-use clap::{Command,Arg};
-use image::extract; 
+use clap::{Arg, Command};
+use image::extract;
 use std::result;
 
 pub fn parse_cli() -> result::Result<&'static str, &'static str> {
@@ -7,32 +7,63 @@ pub fn parse_cli() -> result::Result<&'static str, &'static str> {
         .arg_required_else_help(true)
         .author("ROOM2")
         .version("0.1.0")
-        .about("Runs regular Docker images as Rust VMM containers using an Ubuntu 20.04 kernel.")
-        .subcommand(Command::new("run") // using subcommands so we can expand CLI in the future
-                    .about("Pulls and launches Docker images as runr containers")
-                    .arg(
-                        Arg::new("image")
+        .about("Runs regular Docker images in Rust VMM instances using Ubuntu 20.04 kernel.")
+        .subcommand(
+            Command::new("run") // using subcommands so we can expand CLI in the future
+                .about("Pulls and launches Docker images as runr containers")
+                .arg(
+                    Arg::new("image")
                         .required(true)
                         .value_name("image")
-                        .help("Docker image to build container from"))
-                    .arg(
-                        Arg::new("memory")
+                        .help("Docker image to build container from"),
+                )
+                .arg(
+                    Arg::new("memory")
                         .long("memory")
                         .default_value("256")
-                        .help("Amount of memory (in mib) dedicated to container")))
-        .subcommand(Command::new("pull")
-                    .about("Downloads image from Docker")
-                    .arg(
-                        Arg::new("image")
+                        .help("Amount of memory (in mib) dedicated to container"),
+                ),
+        )
+        .subcommand(
+            Command::new("pull")
+                .about("Downloads image from Docker")
+                .arg(
+                    Arg::new("image")
                         .required(true)
                         .value_name("image")
-                        .help("Docker image to pull")))
+                        .help("Docker image to pull"),
+                ),
+        )
         .get_matches();
+
+    // match matches.subcommand() {
+    //     None => Err("No argument provided"),
+    //     Some(("run", run_match)) => println!("{:?}", run_match),
+    // }
+
+    //let my_struct_string = format!("{:?}", matches.subcommand);
+
+    // matches.subcommand() {
+    //     Some(("run", run_match)) => {println!("Swag")};
+    // }
+
+    //println!("{}", my_struct_string);
+    /*
+            if let Some(c) = matches.get_one::<String>("runr") {
+        println!("Value for -c: {c}");
+    }
+     */
+
+    //println!("Value for --output: {}", matches.get_one::<String>("runr").unwrap());
 
     match matches.subcommand() {
         None => Err("No argument provided"),
-        Some(("run", run_match)) => run_match, 
-        Some(("pull", pull_match)) => pull_match,
+        Some(("run", run_match)) => {
+            let argstr: String = run_match.clone().remove_one("image").expect("failed");
+            println!("{:?}", argstr);
+            return Ok("nice");
+        }
+        Some(("pull", pull_match)) => Ok("nice"),
         Some(_) => Err("Invalid argument"),
     }
 }
