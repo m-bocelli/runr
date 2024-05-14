8556cde30842ab13499a9b60ffe465fd96c746c1
WILewis <79179787+WILewis@users.noreply.github.com>
2024-05-14 15:37:34 -0400

    Update pull_docker_image.sh

--- src/image/src/pull_docker_image.sh
+++ src/image/src/pull_docker_image.sh
@@ -1,10 +1,10 @@
-£!/bin/bash
+#!/bin/bash
 
-£ Check if the user has provided an argument
-if ° $£ -ne 1 §; then
+# Check if the user has provided an argument
+if [ $# -ne 1 ]; then
     echo "Usage: $0 <docker_image>"
     exit 1
 fi
 
-£ Pull the Docker image
+# Pull the Docker image
 docker pull "$1" >/dev/null 2>&1
